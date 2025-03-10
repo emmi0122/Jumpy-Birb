a08a9fe60718860271c208080fb7cdfe63f6d5c3
Jessica <olofsson.jessica@hotmail.com>
2025-03-10 11:27:33 +0100

    add cat and viewport

--- core/src/main/java/io/github/some_example_name/Main.java
+++ core/src/main/java/io/github/some_example_name/Main.java
@@ -1,24 +1,62 @@
 package io.github.some_example_name;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main implements ApplicationListener {
+    Texture backgroundTexture;
+    Texture catTexture;
+    Texture pillarTexture;
+
+    SpriteBatch spriteBatch;
+    FitViewport viewport;
+
+
     @Override
     public void create() {
-        // Prepare your application here.
+        backgroundTexture = new Texture("Spooky-forest.png");
+        catTexture = new Texture("Cat.png");
+
+        spriteBatch = new SpriteBatch();
+        viewport = new FitViewport(8, 5);
     }
 
     @Override
     public void resize(int width, int height) {
-        // Resize your application here. The parameters represent the new window size.
+        viewport.update(width, height, true);
     }
 
     @Override
     public void render() {
-        // Draw your application here.
+        input();
+        logic();
+        draw();
     }
 
+    private void input() {
+
+    }
+
+    private void logic() {
+
+    }
+
+    private void draw() {
+        ScreenUtils.clear(Color.BLACK);
+        viewport.apply();
+        spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
+        spriteBatch.begin();
+
+        spriteBatch.draw(catTexture, 0, 0, 1, 1);
+
+        spriteBatch.end();
+    }
     @Override
     public void pause() {
         // Invoked when your application is paused.
